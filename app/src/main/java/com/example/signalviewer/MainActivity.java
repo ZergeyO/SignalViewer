e3e18cdbf519247ab1458d44e9c8c996e62670ff
ZergeyO <egopjiobachev@gmail.com>
2025-09-21 19:35:11 +0300

    Two activities with logic

--- app/src/main/java/com/example/signalviewer/MainActivity.java
+++ app/src/main/java/com/example/signalviewer/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.signalviewer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,84 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private final String LOG_TAG = "MAIN_ACTIVITY";
+
+    public final String STR_KEY_AMP = "AMPLITUDE_VALUE";
+    public final String STR_KEY_FREQ = "FREQUENCY_VALUE";
+    public final String STR_KEY_PHASE = "PHASE_VALUE";
+
+    private EditText Amplitude;
+    private EditText Frequency;
+    private EditText Phase;
+
+    private Button button_goToGraphs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Amplitude = findViewById(R.id.main_val_Amplitude);
+        Frequency = findViewById(R.id.main_val_Frequency);
+        Phase = findViewById(R.id.main_val_Phase);
+
+        button_goToGraphs = findViewById(R.id.btn_goToGraphs);
+        button_goToGraphs.setOnClickListener(view -> {
+            try {
+                Intent intent = new Intent(this, ViewActivity.class);
+
+                Double Amp = Double.valueOf(Amplitude.getText().toString());
+                Log.d(LOG_TAG, "Amp: " + Amplitude.getText().toString());
+                Double Freq = Double.valueOf(Frequency.getText().toString());
+                Log.d(LOG_TAG, "Freq: " + Frequency.getText().toString());
+                Double Ph = Double.valueOf(Phase.getText().toString());
+                Log.d(LOG_TAG, "Ph: " + Phase.getText().toString());
+
+                intent.putExtra(STR_KEY_AMP, Amp);
+                Log.d(LOG_TAG, String.valueOf(intent.getDoubleExtra(this.STR_KEY_AMP, 0.1)));
+                intent.putExtra(STR_KEY_FREQ, Freq);
+                intent.putExtra(STR_KEY_PHASE, Ph);
+
+                startActivity(intent);
+                Log.d(LOG_TAG, "New Activity");
+            }
+            catch (Exception e) {
+                Toast.makeText(this, "Неккоректный ввод данных", Toast.LENGTH_LONG).show();
+            }
+            ;
         });
+    }
+
+
+    @Override
+    protected void onStart() {
+        Log.d(LOG_TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(LOG_TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(LOG_TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(LOG_TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy");
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/example/signalviewer/ViewActivity.java
@@ -0,0 +1,50 @@
+package com.example.signalviewer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewActivity extends AppCompatActivity {
+    private final String LOG_TAG = "VIEW_ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        Intent intent = getIntent();
+    }
+
+    protected void onStart() {
+        Log.d(LOG_TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(LOG_TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(LOG_TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(LOG_TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy");
+        super.onDestroy();
+    }
+}
+
